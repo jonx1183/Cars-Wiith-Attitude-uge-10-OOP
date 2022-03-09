f5b4436296c4b2341a0b82a25b3d72aff1ebe4eb
jonx1183 <jonx1183@stud.kea.dk>
2022-03-09 10:19:13 +0100

    Initial commit "Ved ikke om jeg har gjort det korrekt"

---
+++ src/Car.java
@@ -0,0 +1,48 @@
+public class Car {
+
+  private String brand;
+  private String model;
+  private String honk;
+
+void setBrand(String b){
+  brand = b;
+}
+
+void setModel(String m){
+  model = m;
+}
+
+void setHonk(String h){
+  honk = h;
+}
+
+  public String getBrand() {
+    return brand;
+  }
+
+  public String getModel() {
+    return model;
+  }
+
+  public String getHonk() {
+    return honk;
+  }
+
+  Car(String m, String b, String h){
+  brand = "BMV";
+  model = "model1";
+  honk = "honk";
+
+}
+
+public void sound(){
+  System.out.println(honk);
+}
+public void drive(){
+  System.out.println(brand + " " + model + " is driving");
+}
+
+public void park(){
+  System.out.println(brand + " " + model + " is parked");
+}
+}
---
+++ src/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+
+  public static void main(String[] args) {
+    Car c = new Car("mbv","model1","honk");
+
+    c.sound();
+    c.drive();
+    c.park();
+
+
+  }
+}
